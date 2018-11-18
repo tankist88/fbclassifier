dec9434763bd4e0c408d6f97f7bd6dd931ad9997
Alexey Ustinov <tankist88@gmail.com>
2018-11-19 00:11:29 +0300

    add index.html

--- classifier/views.py
+++ classifier/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.template import loader
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render())
