268e3813f41d1262a4c275237e6b2250e900bb34
Alexey Ustinov <tankist88@gmail.com>
2018-11-20 23:53:14 +0300

    fix import problem

--- classifier/admin.py
+++ classifier/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import AllowedToken
+from .models import AllowedToken, Class, Feedback
 
 admin.site.register(AllowedToken)
 admin.site.register(Class)
